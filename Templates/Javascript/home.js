c0650c15bdbe456143b97830ca1f6d88fbb92b17
Black 2.o <black2.o@yahoo.com>
2024-10-01 00:35:44 +0600

    Added loader

--- Templates/Javascript/home.js
+++ Templates/Javascript/home.js
@@ -7,7 +7,7 @@ import { getFresnelMat } from "./src/getFresnelMat.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(30, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
@@ -83,3 +83,7 @@ function handleWindowResize () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener('resize', handleWindowResize, false);
+
+
+
+
---
+++ Templates/Javascript/loader.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('main');
+
+    setTimeout(() => {
+        loader.style.display = 'none';
+        content.style.display = 'block';
+    }, 10000);
+});
